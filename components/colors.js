1242e8462210b1b33bbeb8ffb7651b67d3e9d093
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-02-17 23:57:07 -0500

    tried function to change colors (to change themes), failed

--- components/colors.js
+++ components/colors.js
@@ -5,7 +5,7 @@ const medB = '#0e4587';
 const lightB = '#59aae0';
 const highlight = '#fecd09';
 
-const colors = {
+let colors = {
   background: darkB,
   asleepBar: lightB,
   awakeBar: highlight,
@@ -24,11 +24,59 @@ const colors = {
   highlight: '#fecd09',
 }
 
+
 //old colors
 // const darkB = '#4C8C7B';
 // const medB = 'teal';
 // const lightB = '#39BAB1';
 // const highlight = #fecd09;
 // const backgroundDark = '#B1CCC5';
+
+
+export function changeTheme(dark) {
+  console.log('changing theme...');
+  if (dark === true){
+    console.log('dark...!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    colors = {
+      background: darkB,
+      asleepBar: lightB,
+      awakeBar: highlight,
+      data: lightB,
+      descriptions: 'white',
+      axis: 'white',
+      header: medB,
+      triplet: highlight,
+      tabs: lightB,
+      tabSel: highlight,
+      tabText: lightB,
+      tabSelText: darkB,
+      darkB: '#0c2245',
+      medB: '#0e4587',
+      lightB: '#59aae0',
+      highlight: '#fecd09',
+    }
+  }
+  else if (dark === false){
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    colors = {
+      background: 'white',
+      asleepBar: 'white',
+      awakeBar: highlight,
+      data: lightB,
+      descriptions: 'white',
+      axis: 'white',
+      header: medB,
+      triplet: highlight,
+      tabs: lightB,
+      tabSel: highlight,
+      tabText: lightB,
+      tabSelText: darkB,
+      darkB: '#0c2245',
+      medB: '#0e4587',
+      lightB: '#59aae0',
+      highlight: '#fecd09',
+    }
+  }
+};
 
 export default colors;
